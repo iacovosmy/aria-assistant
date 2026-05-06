dc6f4c86e705a24eac5c791015164a9fbb375660
iacovosmy <iacovosmy@gmail.com>
2026-05-06 21:16:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ARIA CONFIG - Azure credentials
 const ARIA_CONFIG = {
   clientId: '20f2158b-7ca7-4ecd-bdce-0ec7ddb668e1',
-  tenantId: '431deb89-a643-4892-a0f3-543329fe3316',
+  tenantId: 'common',
 };
