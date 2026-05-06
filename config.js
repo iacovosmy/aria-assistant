c898490fb1a2622e7668daeb38109412f4869d95
iacovosmy <iacovosmy@gmail.com>
2026-05-06 18:30:39 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// ARIA CONFIG - Fill in your values from the Azure portal
+
+const ARIA_CONFIG = {
+
+  // From Azure Portal -> App registrations -> your app -> Overview
+  clientId: 'PASTE_YOUR_APPLICATION_CLIENT_ID_HERE',
+
+  // From Azure Portal -> App registrations -> your app -> Overview
+  tenantId: 'PASTE_YOUR_DIRECTORY_TENANT_ID_HERE',
+
+};
